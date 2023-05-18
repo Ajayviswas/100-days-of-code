72e732817cbdf058428882d5514b428cd4bdd58a
ajay <ajayviswas19august@gmail.com>
2023-05-18 23:49:31 +0530

    DAY 29 Commit

---
+++ GRAPHS/MIN OPERATIONS TO MAKE  GRAPH CONNECTED.cpp
@@ -0,0 +1,56 @@
+class DisjointSet{
+    public:
+    vector<int>rank,parent,size;
+    DisjointSet(int n)
+    {
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+        size.resize(n+1);
+        for(int i=0;i<=n;i++)
+        {
+            parent[i]=i;
+            size[i]=1;
+        }
+    }
+    
+    int findUpar(int node)
+    {
+        if(node==parent[node])
+        return node;
+        return parent[node]=findUpar(parent[node]);
+    }
+    void unionByRank(int u,int v)
+    {
+        int ulp_u=findUpar(u);
+        int ulp_v=findUpar(v);
+        if(ulp_u==ulp_v)
+        return;
+        if(rank[ulp_u]<rank[ulp_v])
+        parent[ulp_u]=ulp_v;
+        else if(rank[ulp_v]<rank[ulp_u])
+        parent[ulp_v]=ulp_u;
+        else
+        {
+            parent[ulp_v]=ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+    void unionBySize(int u,int v)
+    {
+        int ulp_u=findUpar(u);
+        int ulp_v=findUpar(v);
+        if(ulp_u==ulp_v)
+        return;
+        if(size[ulp_u]<size[ulp_v])
+        {
+            parent[ulp_u]=ulp_v;
+            size[ulp_v]+=size[ulp_u];
+        }
+        else
+        {
+            parent[ulp_v]=ulp_u;
+            size[ulp_u]+=size[ulp_v];
+        }
+    }
+
+};
