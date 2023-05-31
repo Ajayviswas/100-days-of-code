3d84d3ccd10f54b4d6cafdf5ddd310c4c53c8e72
ajay <ajayviswas19august@gmail.com>
2023-05-31 21:13:43 +0530

    DAY 35 Commit

---
+++ GRAPHS/CHEAPEST FLIGHT WITH K STOPS.cpp
@@ -0,0 +1,37 @@
+int CheapestFLight(int n, vector<vector<int>>& flights, int src, int dst, int k)  
+    {
+       vector<pair<int,int>>adj[n];
+       for(auto  it : flights )
+       {
+           adj[it[0]].push_back({it[1],it[2]});
+       }
+       vector<int>dist(n,1e9);
+       queue<pair<int,pair<int,int>>>q;
+       q.push({0,{src,0}});
+       while(!q.empty())
+       {
+            auto it = q.front();
+            q.pop();
+            int stops = it.first;
+            int node = it.second.first;
+            int cost = it.second.second;
+            
+            if(stops>k) continue;
+            for(auto  iter : adj[node])
+            {
+                int adjnode = iter.first;
+                int edw = iter.second;
+                
+                
+                if(cost+edw < dist[adjnode] && stops <= k)
+                {
+                    dist[adjnode] = cost + edw;
+                    q.push({stops+1,{adjnode,cost+edw}});
+                }
+            }
+       }
+       
+       if(dist[dst]==1e9)
+       return  -1;
+       return  dist[dst];
+    }
